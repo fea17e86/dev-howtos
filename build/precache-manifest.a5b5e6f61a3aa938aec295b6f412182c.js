74078607a3bb190d16285518f4eb1c32a77e0f39
fea17e86 <fea17e86@mail.de>
2020-06-18 05:11:40 +0200

    adds react app and redirect

---
+++ build/precache-manifest.a5b5e6f61a3aa938aec295b6f412182c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e5b0c5ced606ebcaf3ad306fddc69c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e85a25d1190de2359808",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7add8f51cd4c5767794b",
+    "url": "/static/js/2.587a6ed6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.587a6ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85a25d1190de2359808",
+    "url": "/static/js/main.e738a347.chunk.js"
+  },
+  {
+    "revision": "f064d9afd50e845fe85c",
+    "url": "/static/js/runtime-main.781ee92c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
